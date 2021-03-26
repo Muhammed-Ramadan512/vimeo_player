77610fd267ac14d6ef9882547efcd6953ce54732
Mohamed Radwan <muhameda.radwan@gmail.com>
2021-03-27 00:23:56 +0200

    Save Work

--- lib/src/quality_links.dart
+++ lib/src/quality_links.dart
@@ -14,15 +14,40 @@ class QualityLinks {
     return getQualitiesAsync();
   }
 
+  // Future<SplayTreeMap> getQualitiesAsync() async {
+  //   try {
+  //     var response = await http
+  //         .get('https://player.vimeo.com/video/' + videoId + '/config');
+  //     var jsonData =
+  //         jsonDecode(response.body)['request']['files']['progressive'];
+  //     SplayTreeMap videoList = SplayTreeMap.fromIterable(jsonData,
+  //         key: (item) => "${item['quality']} ${item['fps']}",
+  //         value: (item) => item['url']);
+  //     return videoList;
+  //   } catch (error) {
+  //     print('=====> REQUEST ERROR: $error');
+  //     return null;
+  //   }
+  // }
+
   Future<SplayTreeMap> getQualitiesAsync() async {
     try {
-      var response = await http
-          .get('https://player.vimeo.com/video/' + videoId + '/config');
-      var jsonData =
-          jsonDecode(response.body)['request']['files']['progressive'];
+      final response =
+          await http.get("https://api.vimeo.com/me/videos/$videoId", headers: {
+        "Authorization": "Bearer 711542deb175eeab25582fa917400463",
+      });
+
+      var jsonData = jsonDecode(response.body)['download'];
       SplayTreeMap videoList = SplayTreeMap.fromIterable(jsonData,
-          key: (item) => "${item['quality']} ${item['fps']}",
-          value: (item) => item['url']);
+          key: (item) => "${item["public_name"]}  ${item["size_short"]} ",
+          value: (item) => item['link']);
+
+      print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+      videoList.forEach((key, value) {
+        print("key: " + key.toString() + "   value: " + value.toString());
+      });
+      print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
       return videoList;
     } catch (error) {
       print('=====> REQUEST ERROR: $error');
